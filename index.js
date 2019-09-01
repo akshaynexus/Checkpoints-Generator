2b13aee0871b282c933297b89964ceed8dad52ab
Akshay <akshaycm@hotmail.com>
2019-09-01 20:07:43 +0300

    Add code to generate checkpoints for PIVX forks and tidy up code

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var explorerAPILink = "http://159.69.33.243:3001/api/";
 var blockspacing = 50000;
 //get this number from last block on explorer
 var currentblock = 581563;
+//Set true or false depending on your requirement
+var fBreadwallet = true;
+var fisPIVXFork = false;
 var i = 0;
 function generateCheckpoints(blockdelay, blockcountcurr) {
     if (i <= blockcountcurr) {
@@ -15,8 +18,8 @@ function generateCheckpoints(blockdelay, blockcountcurr) {
         else if (i == 0) {
             outputdatax = "";
         } else 
-            i += blockdelay;
-        
+            i += blockdelay - 1;
+        //Get block hash
         request(explorerAPILink + 'getblockhash?index=' + i, {
             json: false
         }, (err, res, body) => {
@@ -27,21 +30,35 @@ function generateCheckpoints(blockdelay, blockcountcurr) {
             generateCheckpoints(blockspacing, currentblock);
         });
     }
-    //increment i 
     i += 1;
-
 }
 function ConvertBlockData(currblockhash) {
     var outputdata = ""
     request(explorerAPILink + 'getblock?hash=' + currblockhash, {
         json: true
     }, (err, res, body) => {
-        if (body.height == 0 || body.height < currentblock) { //genesis block
-            outputdata = "{" + body.height + ',uint256("' + body.hash + '"), ' + body.time + ",0x" + body.bits + "},";
-        } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
-            outputdata = "{" + body.height + ',uint256("' + body.hash + '"), ' + body.time + ",0x" + body.bits + "}";
+        if (fBreadwallet) {
+            var hashinquotes = '"' + body.hash + '"';
+            if (body.height == 0 || body.height < currentblock) { //genesis block or after gn
+                outputdata = "{" + body.height + ",uint256(" + hashinquotes + "), " + body.time + ",0x" + body.bits + "},";
+            } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
+                outputdata = "{" + body.height + ",uint256(" + hashinquotes + "), " + body.time + ",0x" + body.bits + "}";
+            }
+            console.log(outputdata)
+        } else if (fisPIVXFork) {
+            var hashinquotes = '"0x' + body.hash + '"';
+            if (body.height == 0) { //genesis block
+                outputdata = "static Checkpoints::MapCheckpoints mapCheckpoints = \n";
+                outputdata += "boost::assign::map_list_of\n";
+                outputdata += "(" + body.height + ",uint256(" + hashinquotes + "))";
+            } else if (body.height < currentblock && body.height > 0) {
+                outputdata = "(" + body.height + ",uint256(" + hashinquotes + "))";
+            } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
+                outputdata = "(" + body.height + ",uint256(" + hashinquotes + "));\n";
+                outputdata += "static const Checkpoints::CCheckpointData data = {\n&mapCheckpoints,\n" + body.time + ",\n" + body.height * 2 + ",\n" + 1440 + "};";
+            }
+            console.log(outputdata)
         }
-        console.log(outputdata)
     });
 }
 generateCheckpoints(blockspacing, currentblock);
