a49d54d3f0af7f7cc610f157da9151d9e08e4502
Akshay <akshaycm@hotmail.com>
2019-09-01 21:01:27 +0300

    Add Support for BlockBook and Bulwark Explorer API's

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const request = require('request');
 //Put Explorer api link here,Iquidus is preffered atm and supported
 var explorerAPILink = "http://159.69.33.243:3001/api/";
+var explorerType = "iquidus"//Supported explorers are Iquidus,Blockbook and Bulwark atm
 var blockspacing = 50000;
 //get this number from last block on explorer
 var currentblock = 581563;
@@ -27,18 +28,17 @@ function generateCheckpoints(blockdelay, blockcountcurr) {
         }, (err, res, body) => {
             currblockhash = body;
             //now that we got blockhash,get block data and parse it to needed info
-            ConvertBlockData(body);
+            ConvertBlockData(body,i);
             //Call the func in of itself,emulating a for loop type situation.
             generateCheckpoints(blockspacing, currentblock);
         });
     }
     i += 1;
 }
-function ConvertBlockData(currblockhash) {
+function ConvertBlockData(currblockhash,blockheight) {
     var outputdata = ""
-    request(explorerAPILink + 'getblock?hash=' + currblockhash, {
-        json: true
-    }, (err, res, body) => {
+    if(explorerType == "iquidus"){
+    request(explorerAPILink + 'getblock?hash=' + currblockhash, {json: true}, (err, res, body) => {
         if (fBreadwallet) {
             var hashinquotes = '"' + body.hash + '"';
             if (body.height == 0 || body.height < currentblock) { //genesis block or after gn
@@ -67,5 +67,70 @@ function ConvertBlockData(currblockhash) {
             console.log(outputdata)
         }
     });
+}
+    else if(explorerType == "bulwark"){
+       request(explorerAPILink + '/block/' + blockheight, {json: true}, (err, res, body) => {
+        var hashinquotes = '"' + body.hash + '"';
+        var timeEpoch = new Date(body.createdAt).getTime() / 1000;//get epoch from createdAt
+        if (fBreadwallet) {
+            if (body.height == 0 || body.height < currentblock) { //genesis block or after gn
+                outputdata = "{" + body.height + ",uint256(" + hashinquotes + "), " + timeEpoch + ",0x" + body.bits + "},";
+            } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
+                outputdata = "{" + body.height + ",uint256(" + hashinquotes + "), " + timeEpoch + ",0x" + body.bits + "}";
+            }
+            console.log(outputdata)
+        } else if (fisPIVXFork) {
+            if (body.height == 0) { //genesis block
+                outputdata = "static Checkpoints::MapCheckpoints mapCheckpoints = \n";
+                outputdata += "boost::assign::map_list_of\n";
+                outputdata += "(" + body.height + ",uint256(" + hashinquotes + "))";
+            } else if (body.height < currentblock && body.height > 0) {
+                outputdata = "(" + body.height + ",uint256(" + hashinquotes + "))";
+            } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
+                outputdata = "(" + body.height + ",uint256(" + hashinquotes + "));\n";
+                outputdata += "static const Checkpoints::CCheckpointData data = {"
+                         +"\n&mapCheckpoints,\n" 
+                         +timeEpoch + ",// * UNIX timestamp of last checkpoint block\n" 
+                         +totaltx+",    // * total number of transactions between genesis and last checkpoint\n" +
+                         "              //   (the tx=... number in the SetBestChain debug.log lines)\n" + 
+                         2000 + "       // * estimated number of transactions per day after checkpoint\n};";
+            }
+            console.log(outputdata)
+        }
+    });
+  }
+
+  else if(explorerType == "blockbook"){
+    request(explorerAPILink + '/block/' + blockheight, {json: true}, (err, res, body) => {
+     var hashinquotes = '"' + body.hash + '"';
+     var timeEpoch = body.time;
+     if (fBreadwallet) {
+         if (body.height == 0 || body.height < currentblock) { //genesis block or after gn
+             outputdata = "{" + body.height + ",uint256(" + hashinquotes + "), " + timeEpoch + ",0x" + body.bits + "},";
+         } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
+             outputdata = "{" + body.height + ",uint256(" + hashinquotes + "), " + timeEpoch + ",0x" + body.bits + "}";
+         }
+         console.log(outputdata)
+     } else if (fisPIVXFork) {
+         if (body.height == 0) { //genesis block
+             outputdata = "static Checkpoints::MapCheckpoints mapCheckpoints = \n";
+             outputdata += "boost::assign::map_list_of\n";
+             outputdata += "(" + body.height + ",uint256(" + hashinquotes + "))";
+         } else if (body.height < currentblock && body.height > 0) {
+             outputdata = "(" + body.height + ",uint256(" + hashinquotes + "))";
+         } else if (body.height == currentblock) { //last block in checkpoints,so dont add , at end of output data
+             outputdata = "(" + body.height + ",uint256(" + hashinquotes + "));\n";
+             outputdata += "static const Checkpoints::CCheckpointData data = {"
+                      +"\n&mapCheckpoints,\n" 
+                      +timeEpoch + ",// * UNIX timestamp of last checkpoint block\n" 
+                      +totaltx+",    // * total number of transactions between genesis and last checkpoint\n" +
+                      "              //   (the tx=... number in the SetBestChain debug.log lines)\n" + 
+                      2000 + "       // * estimated number of transactions per day after checkpoint\n};";
+         }
+         console.log(outputdata)
+     }
+   });
+ }
+
 }
 generateCheckpoints(blockspacing, currentblock);
